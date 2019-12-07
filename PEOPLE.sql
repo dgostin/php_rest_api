b757c434a641c5d7ccfee769ec04d550459f551d
dgostin <58613699+dgostin@users.noreply.github.com>
2019-12-07 17:49:53 -0500

    Add files via upload

---
+++ PEOPLE.sql
@@ -0,0 +1,31 @@
+use sample;
+
+create table IF NOT EXISTS PEOPLE (
+	id INT,
+	first_name VARCHAR(50),
+	last_name VARCHAR(50),
+	email VARCHAR(50),
+	gender VARCHAR(50),
+	title VARCHAR(50),
+	dept VARCHAR(50)
+);
+insert into PEOPLE (id, first_name, last_name, email, gender, title, dept) values (1, 'Duane', 'Burgwin', 'dburgwin0@webnode.com', 'Male', 'Health Coach III', 'Accounting');
+insert into PEOPLE (id, first_name, last_name, email, gender, title, dept) values (2, 'Chloe', 'Blaxill', 'cblaxill1@uol.com.br', 'Female', 'Programmer Analyst I', 'Marketing');
+insert into PEOPLE (id, first_name, last_name, email, gender, title, dept) values (3, 'Julieta', 'Grzesiak', 'jgrzesiak2@reverbnation.com', 'Female', 'Recruiting Manager', 'Accounting');
+insert into PEOPLE (id, first_name, last_name, email, gender, title, dept) values (4, 'Allison', 'Hinkins', 'ahinkins3@miitbeian.gov.cn', 'Female', 'Geological Engineer', 'Legal');
+insert into PEOPLE (id, first_name, last_name, email, gender, title, dept) values (5, 'Hamish', 'Turn', 'hturn4@merriam-webster.com', 'Male', 'Actuary', 'Human Resources');
+insert into PEOPLE (id, first_name, last_name, email, gender, title, dept) values (6, 'Annecorinne', 'Turbill', 'aturbill5@mlb.com', 'Female', 'Actuary', 'Accounting');
+insert into PEOPLE (id, first_name, last_name, email, gender, title, dept) values (7, 'Jaclin', 'Losseljong', 'jlosseljong6@livejournal.com', 'Female', 'Software Engineer IV', 'Marketing');
+insert into PEOPLE (id, first_name, last_name, email, gender, title, dept) values (8, 'Griz', 'Sherington', 'gsherington7@mysql.com', 'Male', 'Internal Auditor', 'Accounting');
+insert into PEOPLE (id, first_name, last_name, email, gender, title, dept) values (9, 'Devin', 'Puddefoot', 'dpuddefoot8@phoca.cz', 'Male', 'Help Desk Technician', 'Human Resources');
+insert into PEOPLE (id, first_name, last_name, email, gender, title, dept) values (10, 'Esdras', 'Carass', 'ecarass9@sbwire.com', 'Male', 'Media Manager I', 'Services');
+insert into PEOPLE (id, first_name, last_name, email, gender, title, dept) values (11, 'Danyelle', 'Cham', 'dchama@ocn.ne.jp', 'Female', 'Operator', 'Human Resources');
+insert into PEOPLE (id, first_name, last_name, email, gender, title, dept) values (12, 'Trish', 'Wason', 'twasonb@naver.com', 'Female', 'Actuary', 'Training');
+insert into PEOPLE (id, first_name, last_name, email, gender, title, dept) values (13, 'Carmita', 'Croucher', 'ccroucherc@netscape.com', 'Female', 'Research Associate', 'Research and Development');
+insert into PEOPLE (id, first_name, last_name, email, gender, title, dept) values (14, 'Penn', 'Twopenny', 'ptwopennyd@bravesites.com', 'Male', 'Environmental Specialist', 'Support');
+insert into PEOPLE (id, first_name, last_name, email, gender, title, dept) values (15, 'Gery', 'Wrightham', 'gwrighthame@phoca.cz', 'Male', 'Environmental Tech', 'Accounting');
+insert into PEOPLE (id, first_name, last_name, email, gender, title, dept) values (16, 'Tish', 'Pinnere', 'tpinneref@dell.com', 'Female', 'Graphic Designer', 'Training');
+insert into PEOPLE (id, first_name, last_name, email, gender, title, dept) values (17, 'Jarrad', 'Rosendall', 'jrosendallg@stanford.edu', 'Male', 'Occupational Therapist', 'Research and Development');
+insert into PEOPLE (id, first_name, last_name, email, gender, title, dept) values (18, 'Rosanne', 'Bushaway', 'rbushawayh@cloudflare.com', 'Female', 'Research Nurse', 'Product Management');
+insert into PEOPLE (id, first_name, last_name, email, gender, title, dept) values (19, 'Hilton', 'Heine', 'hheinei@shinystat.com', 'Male', 'Staff Scientist', 'Legal');
+insert into PEOPLE (id, first_name, last_name, email, gender, title, dept) values (20, 'Maynard', 'Vasyunin', 'mvasyuninj@google.cn', 'Male', 'Technical Writer', 'Support');
