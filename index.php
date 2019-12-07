355c21fbeb7a671ff79ff79537276c92b4b6830e
dgostin <58613699+dgostin@users.noreply.github.com>
2019-12-07 14:11:26 -0500

    Add files via upload

--- index.php
+++ index.php
@@ -28,53 +28,53 @@ $where_clause = $people->get_where_clause_from_url();
 
 $stmt = $people->get_data($where_clause);
 $num = $stmt->rowCount();
- 
-if($num>0){
- 
-	$people=array();
- 
-    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)){
 
-        extract($row);
- 
-        $data=array(
-            "id" => $id,
-            "first_name" => $first_name,
-            "last_name" => $last_name, 
-            "email" => $email, 
-            "gender" => $gender, 
-            "title" => $title, 
-            "dept" => $dept
-        );
- 
-        array_push($people, $data);
-    }
- 
-    // set response code - 200 OK
-    http_response_code(200);
- 
-    // show products data in proper format
-    if ($output_type == 'csv') {
+if($num>0){
+
+	$people=array();
+
+	while ($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+
+		extract($row);
+
+		$data=array(
+			"id" => $id,
+			"first_name" => $first_name,
+			"last_name" => $last_name, 
+			"email" => $email, 
+			"gender" => $gender, 
+			"title" => $title, 
+			"dept" => $dept
+		);
+
+		array_push($people, $data);
+	}
+
+	// set response code - 200 OK
+	http_response_code(200);
+
+	// output proper format
+	if ($output_type == 'csv') {
 		$out = fopen('php://output', 'w');
-    	foreach ($people as $data) {
-	    	fputcsv($out, $data);
-    	}
-   		fclose($out);
-    }
-    else {
-    	echo json_encode($people);
-    }
+		foreach ($people as $data) {
+			fputcsv($out, $data);
+		}
+		fclose($out);
+	}
+	else {
+		echo json_encode($people);
+	}
 
 }
 else{
-     
- 	if ($output_type != 'csv') {
- 		// set response code - 404 Not found
-    	http_response_code(404);
-    	echo json_encode(
-        	array("message" => "No data found.")
-    	);
-    }
+
+	if ($output_type != 'csv') {
+		// set response code - 404 Not found
+		http_response_code(404);
+		echo json_encode(
+			array("message" => "No data found.")
+		);
+	}
 }
 
 ?>
